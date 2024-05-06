4fa03e8196548d18415b20e484758b11faaf9b69
Liren Zhu <liren.zhu.cn@gmail.com>
2024-05-06 13:49:28 -0700

    tweak vim-doge settings

--- lua/plugins/vim-doge.lua
+++ lua/plugins/vim-doge.lua
@@ -4,5 +4,9 @@ return {
   "kkoomen/vim-doge",
   init = function()
     vim.g.doge_doc_standard_python = 'numpy'
+    vim.g['doge_python_settings'] = {
+      single_quotes = 0,
+      omit_redundant_param_types = 0,
+    }
   end,
 }
