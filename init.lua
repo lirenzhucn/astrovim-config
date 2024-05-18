80f6189a002f5c20e952e7f40b065c1aed5b21f0
Liren Zhu <liren.zhu.cn@gmail.com>
2024-05-18 09:10:28 -0700

    add temporary fix for DogeGenerate in cpp files

--- init.lua
+++ init.lua
@@ -3,17 +3,28 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
 
 require "lazy_setup"
 require "polish"
+
+-- this autocmd ensures that for all cpp files, shiftwidth is set to 2
+-- without this autocmd, shiftwidth may be set to 1 (lsp is suspected to be the culprit)
+-- a shiftwidth of 1 will prevent `DogeGenerate` from working properly
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*.c', '*.h', '*.cpp', '*.hpp', '*.cu', '*.cuh' },
+  command = 'setlocal shiftwidth=2'
+})
